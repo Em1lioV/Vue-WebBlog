6eae5e427b0bd560ce22d5922c593f0d3a66d726
Em1lioV <emilio.villagrawt@gmail.com>
2023-09-14 19:52:05 -0300

    blog post composition api

---
+++ src/components/PostList.vue
@@ -0,0 +1,33 @@
+<template >
+    <div class=post-list>
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
+    </div>
+</template>
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue';
+import SinglePost from '../components/SinglePost.vue'
+export default {
+    props: ['posts'],
+    setup(props) {
+        onMounted(() => {
+          console.log('component mounted');  
+        })
+        onUnmounted(() => {
+            console.log('component unmounted');
+        })
+        onUpdated(() => {
+          console.log('component update');  
+        })
+    },
+    components:{SinglePost},
+    mounted() {
+        console.log('mounted using options api');
+    }
+    
+}
+</script>
+<style>
+    
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,22 @@
+<template >
+    <div>
+        <h3>{{ post.title }}</h3>
+        <p v-html="snipped"></p>
+    </div>
+</template>
+<script>
+import { computed } from 'vue';
+
+export default {
+    props:['post'],
+    setup(props) {
+        const snipped = computed(() => {
+            return props.post.body.substring(0,100) + '...'
+        })
+        return {snipped}
+    }
+}
+</script>
+<style lang="">
+    
+</style>
